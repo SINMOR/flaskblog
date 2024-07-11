5b352b3e66a323ee788d8d238139df2345c74d86
Sindet <morrisindet@gmail.com>
2024-07-11 17:24:46 +0300

    added user account and profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf  import FlaskForm
+from flask_wtf.file import FileField,FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, BooleanField , SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -26,6 +28,26 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     
     submit = SubmitField('Login')
+    
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)] ) 
+    
+    email = StringField('Email',  validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+    
+    def validate_username(form, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username taken ! Please choose a  different one.')
+    def validate_email(form, email):
+         if email.data != current_user.email:
+                user = User.query.filter_by(email=email.data).first()
+                if user:
+                    raise ValidationError('Email taken ! Please  choose a different one.')
+            
     
     
     
