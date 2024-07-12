60860cdfc10b0549a08898632b9a78ee8cb77dcd
Sindet <morrisindet@gmail.com>
2024-07-12 17:25:44 +0300

    started content

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf  import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, BooleanField , SubmitField
+from wtforms import StringField, PasswordField, BooleanField , SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -48,6 +48,10 @@ class UpdateAccountForm(FlaskForm):
                 if user:
                     raise ValidationError('Email taken ! Please  choose a different one.')
             
-    
-    
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()] )
+    content = TextAreaField('Content', validators=[DataRequired()] )
+    submit = SubmitField('Post')
     
